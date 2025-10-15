fa0eb2ec64e3532b041daf06a487614fc59d405b
김승태 <tiga1207@naver.com>
2025-10-15 20:01:43 +0900

    [level 0] Title: 주사위의 개수, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] box, int n)
+    {
+        int answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
+
+        return answer;
+    }
+}
