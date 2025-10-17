9edd66b5a53c466a0f2e630d70c96df1b8245f39
김승태 <tiga1207@naver.com>
2025-10-17 16:31:12 +0900

    [level 0] Title: 합성수 찾기, Time: 0.26 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(int n)
+{
+    int answer = 0; // 합성수 개수
+    for(int i= 1; i<=n;i++)
+    {
+        int count = 0;
+        for(int j = 1; j<=i; j++)
+        {
+            if(i%j ==0)
+            {
+                count++;
+            }
+        }
+        if (count >= 3)
+            answer++;
+    }
+    return answer;
+}
+}
